1cfae2c5528eb7181744822d2b52d9ce0acdaeff
Joe Woodson <contact@joewoodson.de>
2017-06-21 00:19:57 +0200

    add intro and services sections

---
+++ templates/services.php
@@ -0,0 +1,15 @@
+<section class="services">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 df center-center">
+        <h5>Vorsorge</h5>
+      </div>
+      <div class="col-md-4 df center-center">
+        <h5>Nachsorge</h5>
+      </div>
+      <div class="col-md-4 df center-center">
+        <h5>Rückbildungskurse</h5>
+      </div>
+    </div>
+  </div>
+</section>
