b156701d0eb6abca6d6239b1ff1e40abadb74f61
Joe Woodson <contact@joewoodson.de>
2017-06-22 20:45:11 +0200

    style text

--- templates/services.php
+++ templates/services.php
@@ -2,12 +2,12 @@
   <div class="container">
     <div class="row">
       <div class="col-md-4 df flex-col center-center service">
-        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/lotus.png" alt="vorsorge icon">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/belly.png" alt="vorsorge icon">
         <h5>Vorsorge</h5>
         <p>Lorem ipsum dolor sit amet, habeo honestatis sed ne, his eleifend moderatius cu. Aperiam discere convenire id mel.</p>
       </div>
       <div class="col-md-4 df flex-col center-center service">
-        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/belly.png" alt="nachsorge icon">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/lotus-new.png" alt="nachsorge icon">
         <h5>Nachsorge</h5>
         <p>Lorem ipsum dolor sit amet, habeo honestatis sed ne, his eleifend moderatius cu. Aperiam discere convenire id mel.</p>
       </div>
