79e81bca02adea005da3a6d0db96c565cbc3dd4c
Joe Woodson <contact@joewoodson.de>
2017-06-22 20:13:46 +0200

    style services

--- template-custom.php
+++ template-custom.php
@@ -11,7 +11,7 @@
         <?php get_template_part('templates/content', 'page'); ?>
       </div>
       <div class="col-md-4">
-        <img class="img-responsive img-thumbnail headshot" src="<?php echo get_template_directory_uri(); ?>/dist/images/headshot-min.jpg" alt="">
+        <img class="img-responsive img-thumbnail headshot" src="<?php echo get_template_directory_uri(); ?>/dist/images/headshot-min.jpg" alt="amelie headshot">
       </div>
     </div>
   </section>
--- templates/services.php
+++ templates/services.php
@@ -1,14 +1,20 @@
 <section class="services">
   <div class="container">
     <div class="row">
-      <div class="col-md-4 df center-center">
+      <div class="col-md-4 df flex-col center-center service">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/lotus.png" alt="vorsorge icon">
         <h5>Vorsorge</h5>
+        <p>Lorem ipsum dolor sit amet, habeo honestatis sed ne, his eleifend moderatius cu. Aperiam discere convenire id mel.</p>
       </div>
-      <div class="col-md-4 df center-center">
+      <div class="col-md-4 df flex-col center-center service">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/belly.png" alt="nachsorge icon">
         <h5>Nachsorge</h5>
+        <p>Lorem ipsum dolor sit amet, habeo honestatis sed ne, his eleifend moderatius cu. Aperiam discere convenire id mel.</p>
       </div>
-      <div class="col-md-4 df center-center">
+      <div class="col-md-4 df flex-col center-center service">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/gymnastics.png" alt="rueckbildungskurse icon">
         <h5>Rückbildungskurse</h5>
+        <p>Lorem ipsum dolor sit amet, habeo honestatis sed ne, his eleifend moderatius cu. Aperiam discere convenire id mel.</p>
       </div>
     </div>
   </div>
