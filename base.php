6889644d7798a03a48b914e29fd7aa2849158f72
Joe Woodson <contact@joewoodson.de>
2017-06-24 17:03:18 +0200

    style testimonials

--- base.php
+++ base.php
@@ -39,9 +39,8 @@ use Roots\Sage\Wrapper;
     <script type="text/javascript">
       jQuery(document).ready(function(){
         jQuery('#testimonials-slider').slick({
-          autoplay: true,
+          // autoplay: true,
           autoplaySpeed: 7000,
-          speed: 700,
           prevArrow: '<i class="fa fa-chevron-left slick-prev" aria-hidden="true"></i>',
           nextArrow: '<i class="fa fa-chevron-right slick-next" aria-hidden="true"></i>'
         });
--- templates/testimonials.php
+++ templates/testimonials.php
@@ -1,9 +1,14 @@
 <section class="testimonials">
   <div class="text-center">
     <div id="testimonials-slider">
-      <div class="testimonial-slide testimonial-1"><h3></h3></div>
-      <div class="testimonial-slide testimonial-2"><h3></h3></div>
-      <div class="testimonial-slide testimonial-3"><h3></h3></div>
+      <div class="df flex-col center-center testimonial-slide testimonial-1">
+        <div class="testimonial">
+          <h3><span class="quotation-mark">&ldquo;</span>Amelie is the greatest, nobody catches babies like her!<span class="quotation-mark">&rdquo;</span></h3>
+          <h5 class="attribution">Jane Doe, Real Client</h5>
+        </div>
+      </div>
+      <div class="df flex-col center-center testimonial-slide testimonial-2"><h3>2</h3></div>
+      <div class="df flex-col center-center testimonial-slide testimonial-3"><h3>3</h3></div>
     </div>
   </div>
 </section>
