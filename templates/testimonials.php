a53611243d71d523c6f4122dccf12b609121036a
Joe Woodson <contact@joewoodson.de>
2017-06-24 17:13:27 +0200

    add other placeholder testimonials

--- templates/testimonials.php
+++ templates/testimonials.php
@@ -7,8 +7,19 @@
           <h5 class="attribution">Jane Doe, Real Client</h5>
         </div>
       </div>
-      <div class="df flex-col center-center testimonial-slide testimonial-2"><h3>2</h3></div>
-      <div class="df flex-col center-center testimonial-slide testimonial-3"><h3>3</h3></div>
+      <div class="df flex-col center-center testimonial-slide testimonial-2">
+        <div class="testimonial">
+          <h3><span class="quotation-mark">&ldquo;</span>I got pregnant again just so I could hang out with Hebammelie some more. That's how cool she is!<span class="quotation-mark">&rdquo;</span></h3>
+          <h5 class="attribution">Jane Reh, Actual Pregnant Lady</h5>
+        </div>
+      </div>
+      <div class="df flex-col center-center testimonial-slide testimonial-3">
+        <div class="testimonial">
+          <h3><span class="quotation-mark">&ldquo;</span>You'd have to be stupid not to have your baby with Amelie. You're not stupid, are you?<span class="quotation-mark">&rdquo;</span></h3>
+          <h5 class="attribution">Jane Mi, Disgruntled Mother</h5>
+        </div>
+      </div>
+      </div>
     </div>
   </div>
 </section>
